f63d49c7e1ceb07b5e54aa42bba5ba0970861954
stephan.com <stephan@stephan.com>
2016-02-22 06:14:06 -0500

    Revert "DataTrader broken up into files - but failing with autoload error, only on some tests?"
    This reverts commit 448f35f9a3a9eaf04f396a3eae3d8ad13b95b239.

--- lib/data_trader/data_trader.rb
+++ lib/data_trader/data_trader.rb
@@ -1,9 +1,54 @@
+require 'csv'
 module DataTrader
+  # importer modules must implement a rows method returning an array of hashes
+  # or at least some sort of enumerable that returns hashes
+  # they typically accept as input a string of data, but this need not be so
+  module Importer
+    class CSVImporter
+      def initialize(data)
+        @data = data
+      end
+
+      def rows
+        CSV.parse(@data, headers: true).map(&:to_hash)
+      end
+    end
+  end
+
+  # transformer modules must implement a transform_row class method
+  # which takes a hash as input and returns a different hash
+  module Transformer  
+    class Base
+      def self.transform_row(row)
+        row
+      end
+    end
+  end
+
+  # presenter modules accept an array of hashes at initialization
+  # they must implement a "present" class method which accepts
+  # an array of hashes and returns or does whatever is expected
+  # e.g. a JSONpresenter returns a JSON string
+  # another presenter might create records, etc.
+  module Presenter
+    class Base
+      def self.present(hashes)
+        hashes
+      end
+    end
+
+    class JSONPresenter < Base
+      def self.present
+        hashes.to_json
+      end
+    end
+  end
+
   class Agent
     def initialize(importer, args={})
       @importer = importer
-      @transformer = args[:transformer] || DataTrader::Transformer::Base.new
-      @presenter = args[:presenter] || DataTrader::Presenter::Base.new
+      @transformer = args[:transformer] || Transformer::Base
+      @presenter = args[:presenter] || Presenter::Base
     end
 
     def trade
--- lib/data_trader/transformer/base.rb
+++
@@ -1,9 +0,0 @@
-# transformer modules must implement a transform_row class method
-# which takes a hash as input and returns a different hash
-module DataTrader::Transformer 
-  class Base
-    def transform_row(row)
-      row
-    end
-  end
-end
